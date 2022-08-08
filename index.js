2277de66ac64d5984524944d3ed858cda60524ee
Piaaaaa <susinurlaelasari2@gmail.com>
2022-08-08 22:02:17 +0700

    Lo

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 const express = require("express");
-const app = express();
+const eru = express();
 const port = 3000;
 const api = require("./route/api");
 const short = require("./route/short");
-app.use(express.static(__dirname + "/public"));
-app.use("/api", api);
-app.use("/short", short);
-// Landing page
-app.get("/", (req, res) => {
+
+eru.use(express.static(__dirname + "/public"));
+eru.use("/api", api);
+eru.use("/short", short);
+//     Landing page
+
+eru.get("/", (req, res) => {
   res.sendFile("views/landing.html", { root: __dirname });
 });
-
-app.get("/docs", (req, res) => {
+// docs
+eru.get("/docs", (req, res) => {
   res.sendFile("views/docs.html", { root: __dirname });
 });
-app.use("/", (req, res) => {
+// wallpaper
+eru.get("/wallpaper", (req, res) => {
+  res.sendFile("views/wallpaper.html", { root: __dirname });
+});
+// 404
+eru.use("/", (req, res) => {
   res.status(404);
   res.sendFile("views/404.html", { root: __dirname });
 });
-app.listen(port, () => {
+eru.get("/tes", (req, res) => {
+  const { spawn } = require("child_process");
+  const py = spawn("python", ["tes.py"]);
+  py.stdout.on("data", (data) => {
+    // console.log(data.toString());
+    res.send(data.toString());
+  });
+  py.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+  });
+  // py.on("close", (code) => {
+  //   console.log(`child process exited with code ${code}`);
+  // });
+});
+eru.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
